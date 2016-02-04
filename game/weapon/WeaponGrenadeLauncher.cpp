9c21dbb4c4a609033d4747233a669fa006b71afb
Warren Lee <wpl2@njit.edu>
2016-02-04 06:56:23 -0500

    game/weapon/WeaponGrenadeLauncher.cpp --3 grenades launched per shot grenadeparty/def/player.def --spawn with grenade launcher at max ammo --raised max ammo to 600 grenadeparty/def/weapons/grenadelauncher.def --raised fire rate to .2 --set spread to 5 --set launch direction to (0, 0, 0) --increased clip size to 50 --lowered gravity on projectiles to .25 --shortened projectile fuse to 1.5 --increased direct and splash damage by 4x and splash knockback by 2x

--- game/weapon/WeaponGrenadeLauncher.cpp
+++ game/weapon/WeaponGrenadeLauncher.cpp
@@ -145,7 +145,7 @@ stateResult_t rvWeaponGrenadeLauncher::State_Fire ( const stateParms_t& parms ) 
 	switch ( parms.stage ) {
 		case STAGE_INIT:
 			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-			Attack ( false, 1, spread, 0, 1.0f );
+			Attack ( false, 3, spread, 0, 1.0f );
 			PlayAnim ( ANIMCHANNEL_ALL, GetFireAnim(), 0 );	
 			return SRESULT_STAGE ( STAGE_WAIT );
 	
